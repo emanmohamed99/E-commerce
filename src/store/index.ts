374f6acaeda9a3fea07a0f0a0cf6d62305a1d863
eman <emansalah1999@gmail.com>
2023-12-14 14:44:54 +0200

    change presist to cart and auth

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,10 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // Defaults to localStorage for web
-import category from "./category/categorySlice";
-import products from "./product/productSlice";
-import cart from "./cart/cartSlice";
-import auth from "./auth/authSlice";
-
+import category from './category/categorySlice';
+import products from './product/productSlice';
+import cart from './cart/cartSlice';
+import auth from './auth/authSlice';
 
 const rootReducer = combineReducers({
   category,
@@ -14,20 +13,21 @@ const rootReducer = combineReducers({
   auth,
 });
 
+// Whitelist 'auth' and 'cart' for persistence
 const persistConfig = {
   key: 'root',
   storage,
+  whitelist: ['auth', 'cart'], // Only 'auth' and 'cart' will be persisted
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
- middleware: (getDefaultMiddleware) =>
- getDefaultMiddleware({
-  
-   serializableCheck: false,
- }),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 const persistedStore = persistStore(store);

0ca840ebaeed666f15f6b46702b3a13bc1a9d546
eman <emansalah1999@gmail.com>
2023-12-30 22:01:18 +0200

    change

--- src/Layouts/ProfileLayout/ProfileLayout.tsx
+++ src/Layouts/ProfileLayout/ProfileLayout.tsx
@@ -15,13 +15,14 @@ const Profile = () => {
     <div className="m-2" >
       <div className="container-fluid ">
         <div className="row flex-nowrap">
+      
           <div
             className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark bg-opacity-75 rounded collapse show"
             id="sidebar"
             style={{ height: "30vh" }}
           >
             <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white ">
-          
+          <div className="mt-3 d-sm-block d-none">{t("Menu")}</div>
 
               <ul
                 className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start p-0 "
